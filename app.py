9cb080cbb6dc2d71d15709efb61efb6313c08b60
Julio Alejandro Hincapie Correa <juhincapiec@unal.edu.co>
2021-01-28 21:50:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -191,51 +191,29 @@ def webhook():
                     recipient_id = messaging_event['recipient']['id']  # el facebook ID de la pagina que recibe (tu pagina)
                     message_text = messaging_event['message']['text']  # el texto del mensaje
 
-                    class ChatBot():
-                        global chatbot_response
-                        negative_responses = ("no", "nope", "nah", "naw", "not a chance", "sorry")
-                        exit_commands = ("quit", "pause", "exit", "goodbye", "bye", "later", "stop")
-                        #Method to start the conversation
-                        def start_chat(self):
-                            user_response = message_text #input("Hi, I'm a chatbot trained on random dialogs. Would you like to chat with me?\n")
-                            if user_response in self.negative_responses:
-                                print("Ok, have a great day!")
-                                return
-                            self.chat(user_response)
-                        #Method to handle the conversation
-                        def chat(self, reply):
-                            while not self.make_exit(reply):
-                                reply = input(self.generate_response(reply)+"\n")
-                            
-                        #Method to convert user input into a matrix
-                        def string_to_matrix(self, user_input):
-                            tokens = re.findall(r"[\w']+|[^\s\w]", user_input)
-                            user_input_matrix = numpy.zeros((1, max_encoder_seq_length, num_encoder_tokens),dtype='float32')
+                    if message_text == "Hola":
+                        send_message(sender_id, "Hola, escribe una pregunta")
+
+                    else:
+
+                        def string_to_matrix(pregunta):
+                            tokens = re.findall(r"[\w']+|[^\s\w]", pregunta)
+                            user_input_matrix = numpy.zeros((1, max_encoder_seq_length, num_encoder_tokens), dtype='float32')
                             for timestep, token in enumerate(tokens):
                                 if token in input_features_dict:
                                     user_input_matrix[0, timestep, input_features_dict[token]] = 1.
-                            return user_input_matrix
-                        
+                                return user_input_matrix
+    
                         #Method that will create a response using seq2seq model we built
-                        def generate_response(self, user_input):
-                            global chatbot_response
-                            input_matrix = self.string_to_matrix(user_input)
+                        def generate_response(pregunta):
+                            input_matrix = string_to_matrix(pregunta)
                             chatbot_response = decode_response(input_matrix)
-                            #Remove <START> and <END> tokens from chatbot_response
+                        # Remove <START> and <END> tokens from chatbot_response
                             chatbot_response = chatbot_response.replace("<START>",'')
                             chatbot_response = chatbot_response.replace("<END>",'')
                             return chatbot_response
-                        #Method to check for exit commands
-                        def make_exit(self, reply):
-                            for exit_command in self.exit_commands:
-                                if exit_command in reply:
-                                    print("Ok, have a great day!")
-                                    return True
-                            return False
-                    chatbot = ChatBot()
-                    chatbot.start_chat()
-
-                    send_message(sender_id, chatbot_response)
+                        
+                        send_message(sender_id, generate_response(message_text))
                        
                 if messaging_event.get('delivery'):  # confirmacion de delivery
                     pass
