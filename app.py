215c9e1e727caaa30c9c9db85ed38902b9a5bdbc
Julio Alejandro Hincapie Correa <juhincapiec@unal.edu.co>
2021-01-28 19:24:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ data_path = os.path.join(sys.path[0],"PreguntasFinal.csv")
 data_path2 = os.path.join(sys.path[0],"RespuestasFinal.csv")
 
 # Defining lines as a list of each line
-with open(data_path, 'r') as f:
+with open(data_path, 'r',encoding='latin-1') as f:
   lines = f.read().split('\n')
-with open(data_path2, 'r') as f:
+with open(data_path2, 'r',encoding='latin-1') as f:
   lines2 = f.read().split('\n')
 lines = [re.sub(r"\[\w+\]",'hi',line) for line in lines]
 lines = [" ".join(re.findall(r"\w+",line)) for line in lines]
@@ -29,79 +29,79 @@ input_docs = []
 target_docs = []
 input_tokens = set()
 target_tokens = set()
-for line in pairs[:400]:
-  input_doc, target_doc = line[0], line[1]
-  # Appending each input sentence to input_docs
-  input_docs.append(input_doc)
-  # Splitting words from punctuation  
-  target_doc = " ".join(re.findall(r"[\w']+|[^\s\w]", target_doc))
-  # Redefine target_doc below and append it to target_docs
-  target_doc = '<START> ' + target_doc + ' <END>'
-  target_docs.append(target_doc)
+# for line in pairs[:400]:
+#   input_doc, target_doc = line[0], line[1]
+#   # Appending each input sentence to input_docs
+#   input_docs.append(input_doc)
+#   # Splitting words from punctuation  
+#   target_doc = " ".join(re.findall(r"[\w']+|[^\s\w]", target_doc))
+#   # Redefine target_doc below and append it to target_docs
+#   target_doc = '<START> ' + target_doc + ' <END>'
+#   target_docs.append(target_doc)
     
-  # Now we split up each sentence into words and add each unique word to our vocabulary set
-  for token in re.findall(r"[\w']+|[^\s\w]", input_doc):
-    if token not in input_tokens:
-      input_tokens.add(token)
-  for token in target_doc.split():
-    if token not in target_tokens:
-      target_tokens.add(token)
-input_tokens = sorted(list(input_tokens))
-target_tokens = sorted(list(target_tokens))
-num_encoder_tokens = len(input_tokens)
-num_decoder_tokens = len(target_tokens)
+#   # Now we split up each sentence into words and add each unique word to our vocabulary set
+#   for token in re.findall(r"[\w']+|[^\s\w]", input_doc):
+#     if token not in input_tokens:
+#       input_tokens.add(token)
+#   for token in target_doc.split():
+#     if token not in target_tokens:
+#       target_tokens.add(token)
+# input_tokens = sorted(list(input_tokens))
+# target_tokens = sorted(list(target_tokens))
+# num_encoder_tokens = len(input_tokens)
+# num_decoder_tokens = len(target_tokens)
 
-input_features_dict = dict(
-    [(token, i) for i, token in enumerate(input_tokens)])
-target_features_dict = dict(
-    [(token, i) for i, token in enumerate(target_tokens)])
+# input_features_dict = dict(
+#     [(token, i) for i, token in enumerate(input_tokens)])
+# target_features_dict = dict(
+#     [(token, i) for i, token in enumerate(target_tokens)])
 
-reverse_input_features_dict = dict(
-    (i, token) for token, i in input_features_dict.items())
-reverse_target_features_dict = dict(
-    (i, token) for token, i in target_features_dict.items())
+# reverse_input_features_dict = dict(
+#     (i, token) for token, i in input_features_dict.items())
+# reverse_target_features_dict = dict(
+#     (i, token) for token, i in target_features_dict.items())
 
 
-max_encoder_seq_length = max([len(re.findall(r"[\w']+|[^\s\w]", input_doc)) for input_doc in input_docs])
-max_decoder_seq_length = max([len(re.findall(r"[\w']+|[^\s\w]", target_doc)) for target_doc in target_docs])
+# max_encoder_seq_length = max([len(re.findall(r"[\w']+|[^\s\w]", input_doc)) for input_doc in input_docs])
+# max_decoder_seq_length = max([len(re.findall(r"[\w']+|[^\s\w]", target_doc)) for target_doc in target_docs])
 
-encoder_input_data = numpy.zeros(
-    (len(input_docs), max_encoder_seq_length, num_encoder_tokens),
-    dtype='float32')
-decoder_input_data = numpy.zeros(
-    (len(input_docs), max_decoder_seq_length, num_decoder_tokens),
-    dtype='float32')
-decoder_target_data = numpy.zeros(
-    (len(input_docs), max_decoder_seq_length, num_decoder_tokens),
-    dtype='float32')
+# encoder_input_data = numpy.zeros(
+#     (len(input_docs), max_encoder_seq_length, num_encoder_tokens),
+#     dtype='float32')
+# decoder_input_data = numpy.zeros(
+#     (len(input_docs), max_decoder_seq_length, num_decoder_tokens),
+#     dtype='float32')
+# decoder_target_data = numpy.zeros(
+#     (len(input_docs), max_decoder_seq_length, num_decoder_tokens),
+#     dtype='float32')
 
-for line, (input_doc, target_doc) in enumerate(zip(input_docs, target_docs)):
-    for timestep, token in enumerate(re.findall(r"[\w']+|[^\s\w]", input_doc)):
-        #Assign 1. for the current line, timestep, & word in encoder_input_data
-        encoder_input_data[line, timestep, input_features_dict[token]] = 1.
+# for line, (input_doc, target_doc) in enumerate(zip(input_docs, target_docs)):
+#     for timestep, token in enumerate(re.findall(r"[\w']+|[^\s\w]", input_doc)):
+#         #Assign 1. for the current line, timestep, & word in encoder_input_data
+#         encoder_input_data[line, timestep, input_features_dict[token]] = 1.
     
-    for timestep, token in enumerate(target_doc.split()):
-        decoder_input_data[line, timestep, target_features_dict[token]] = 1.
-        if timestep > 0:
-            decoder_target_data[line, timestep - 1, target_features_dict[token]] = 1.
+#     for timestep, token in enumerate(target_doc.split()):
+#         decoder_input_data[line, timestep, target_features_dict[token]] = 1.
+#         if timestep > 0:
+#             decoder_target_data[line, timestep - 1, target_features_dict[token]] = 1.
 
-#Dimensionality
-dimensionality = 256
-#The batch size and number of epochs
-batch_size = 10
-epochs = 600
-#Encoder
-encoder_inputs = tensorflow.keras.layers.Input(shape=(None, num_encoder_tokens))
-encoder_lstm = tensorflow.keras.layers.LSTM(dimensionality, return_state=True)
-encoder_outputs, state_hidden, state_cell = encoder_lstm(encoder_inputs)
-encoder_states = [state_hidden, state_cell]
+# #Dimensionality
+# dimensionality = 256
+# #The batch size and number of epochs
+# batch_size = 10
+# epochs = 600
+# #Encoder
+# encoder_inputs = tensorflow.keras.layers.Input(shape=(None, num_encoder_tokens))
+# encoder_lstm = tensorflow.keras.layers.LSTM(dimensionality, return_state=True)
+# encoder_outputs, state_hidden, state_cell = encoder_lstm(encoder_inputs)
+# encoder_states = [state_hidden, state_cell]
 
-#Decoder
-decoder_inputs = tensorflow.keras.layers.Input(shape=(None, num_decoder_tokens))
-decoder_lstm = tensorflow.keras.layers.LSTM(dimensionality, return_sequences=True, return_state=True)
-decoder_outputs, decoder_state_hidden, decoder_state_cell = decoder_lstm(decoder_inputs, initial_state=encoder_states)
-decoder_dense = tensorflow.keras.layers.Dense(num_decoder_tokens, activation='softmax')
-decoder_outputs = decoder_dense(decoder_outputs)
+# #Decoder
+# decoder_inputs = tensorflow.keras.layers.Input(shape=(None, num_decoder_tokens))
+# decoder_lstm = tensorflow.keras.layers.LSTM(dimensionality, return_sequences=True, return_state=True)
+# decoder_outputs, decoder_state_hidden, decoder_state_cell = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+# decoder_dense = tensorflow.keras.layers.Dense(num_decoder_tokens, activation='softmax')
+# decoder_outputs = decoder_dense(decoder_outputs)
 
 
 # training_model = tensorflow.keras.models.load_model("training_model.h5")
